d36c4d0d033a41d525ecae8f250ae1c53c46913c
15hack <15hack@riseup.net>
2019-05-02 15:11:11 +0200

    pep8

--- webarchive.py
+++ webarchive.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import os
+import signal
+import sys
+from urllib.parse import urlparse
+
 import requests
 import savepagenow
-import signal
-from urllib.parse import urlparse
 import urllib3
-import sys
-from glob import glob
 
 urllib3.disable_warnings()
 requests.packages.urllib3.disable_warnings()
@@ -14,6 +14,7 @@ requests.packages.urllib3.disable_warnings()
 web_archive_ok = "data/webarchive_ok.txt"
 web_archive_ko = "data/webarchive_ko.txt"
 txt_links = "data/links.txt"
+
 
 def get_file(name, field=None):
     lines = []
@@ -40,26 +41,31 @@ links = [l for l in get_file(txt_links) if l not in done]
 f_ok = open(web_archive_ok, "a+")
 f_ko = open(web_archive_ko, "a+")
 
+
 def signal_handler(signal, frame):
     f_ok.close()
     f_ko.close()
     sys.exit(0)
+
+
 signal.signal(signal.SIGINT, signal_handler)
+
 
 def save(l, archive_url):
     f_ok.write("%s\n" % (l))
     print(archive_url+"\n")
     done.add(l)
 
-count_dom={}
+
+count_dom = {}
 for l in ok_list:
     dom = urlparse(l).netloc
     ok, ko = count_dom.get(dom, (0, 0))
-    count_dom[dom]=(ok+1, ko)
+    count_dom[dom] = (ok+1, ko)
 for l in ko_list:
     dom = urlparse(l).netloc
     ok, ko = count_dom.get(dom, (0, 0))
-    count_dom[dom]=(ok, ko+1)
+    count_dom[dom] = (ok, ko+1)
 total = len(links)
 
 
@@ -68,7 +74,7 @@ for l in reversed(links):
     if l not in done:
         dom = urlparse(l).netloc
         ok, ko = count_dom.get(dom, (0, 0))
-        if ok==0 and ko>10:
+        if ok == 0 and ko > 10:
             continue
         print("%d %s" % (total, l))
         try:
@@ -84,7 +90,7 @@ for l in reversed(links):
             f_ko.write(l+" "+txt)
             print(txt)
             ko = ko + 1
-        count_dom[dom]=(ok, ko)
+        count_dom[dom] = (ok, ko)
 
 f_ok.close()
 f_ko.close()
