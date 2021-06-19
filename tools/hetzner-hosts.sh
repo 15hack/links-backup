3da18d374167622f1c7c7f39ead354e4ed881dd2
15hack <15hack@riseup.net>
2021-06-19 13:16:19 +0200

    crear hosts para hetzner

--- tools/hetzner-hosts.sh
+++ tools/hetzner-hosts.sh
@@ -2,6 +2,10 @@
 RIP="192.168.52.102"
 curl -s "https://raw.githubusercontent.com/15hack/web-backup/main/out/links.txt" | \
 cut -d'/' -f3 | sort | uniq | while read DOM; do
+  if [ "$DOM" == "www.tomalatele.tv" ]; then
+    echo "${RIP} $DOM"
+    continue
+  fi
   ST=$(curl -kLs -o /dev/null -w "%{http_code}" "http://$DOM")
   if [ "$ST" != "200" ]; then
     ST=$(curl --resolve "${DOM}:443:${RIP}" --resolve "${DOM}:80:${RIP}" -kLs -o /dev/null -w "%{http_code}" "http://$DOM")
