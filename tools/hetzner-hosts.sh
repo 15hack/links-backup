4e7e25e317f9386683d64e747102de4abde6bddc
15hack <15hack@riseup.net>
2021-06-19 13:14:33 +0200

    crear hosts para hetzner

---
+++ tools/hetzner-hosts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+RIP="192.168.52.102"
+curl -s "https://raw.githubusercontent.com/15hack/web-backup/main/out/links.txt" | \
+cut -d'/' -f3 | sort | uniq | while read DOM; do
+  ST=$(curl -kLs -o /dev/null -w "%{http_code}" "http://$DOM")
+  if [ "$ST" != "200" ]; then
+    ST=$(curl --resolve "${DOM}:443:${RIP}" --resolve "${DOM}:80:${RIP}" -kLs -o /dev/null -w "%{http_code}" "http://$DOM")
+    if [ "$ST" == "200" ]; then
+      echo "${RIP} $DOM"
+    else
+      echo "# $DOM status_code = $ST"
+    fi
+  fi
+done
